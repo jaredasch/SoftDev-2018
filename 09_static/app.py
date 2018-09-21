394b8d511cba94e017a4a8267d889322a6181890
jaredasch1 <jaredasch1@gmail.com>
2018-09-21 06:19:36 -0400

    added work 09

---
+++ 09_static/app.py
@@ -0,0 +1,11 @@
+# Team Snowglobe - Jared Asch, Ryan Aday
+# SoftDev pd1
+# K09 -- Solidify
+# 2018-09-21
+
+from flask import Flask
+app = Flask(__name__)
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run()
