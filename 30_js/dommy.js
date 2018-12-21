afa62c01edecfd5cc537e66151c176ce33029474
Jared Asch <jaredasch1@gmail.com>
2018-12-20 21:49:04 -0500

    bug fixes

--- 30_js/dommy.js
+++ 30_js/dommy.js
@@ -8,26 +8,29 @@ let fib = (n) => {
 itemList = document.getElementById("thelist");
 
 document.getElementById("b").addEventListener("click", function(e){
-    let new_li = document.createElement("li");
-    new_li.innerHTML = "New list element";
-    itemList.appendChild(new_li);
+    let newLi = document.createElement("li");
+    newLi.innerHTML = "New list element";
+    itemList.appendChild(newLi);
+    addListListeners(newLi);
 });
 
-itemList.addEventListener("click", function(e){
-    e.target.remove();
-});
-
+let addListListeners = (li) => {
+    li.addEventListener("mouseenter", function(e){
+        document.getElementById("h").innerHTML = this.innerHTML;
+    });
+    li.addEventListener("mouseout", function(e){
+        document.getElementById("h").innerHTML = "Hello world";
+    });
+    li.addEventListener("click", function(e){
+        this.remove();
+    });
+}
 
 var lis = document.getElementsByTagName("li");
 for(var i = 0; i < lis.length; i++){
-    lis[i].addEventListener("mouseenter", function(e){
-	document.getElementById("h").innerHTML = e.target.innerHTML;
-    });
-    lis[i].addEventListener("mouseout", function(e){
-	document.getElementById("h").innerHTML = "Hello world";
-    });
+    addListListeners(lis[i]);
 }
-			  
+
 var fib_num = 0;
 
 document.getElementById("fb").addEventListener("click", function(e){
